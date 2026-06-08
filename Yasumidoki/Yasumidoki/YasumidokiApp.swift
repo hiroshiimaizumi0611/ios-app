1d11bda0db6f1cd16e34cebe1e03b452db4ac2c8
hiroshiimaizumi0611 <114396330+hiroshiimaizumi0611@users.noreply.github.com>
2026-06-08 09:53:08 +0900

    Create Yasumidoki SwiftUI app project

---
+++ Yasumidoki/Yasumidoki/YasumidokiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YasumidokiApp.swift
+//  Yasumidoki
+//
+//  Created by hiroshi imaizumi on 2026/06/08.
+//
+
+import SwiftUI
+
+@main
+struct YasumidokiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
